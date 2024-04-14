c2c6e00f0f85c60e6a478611af3ea16b08654237
unknown <md.r.a.n.786@gmail.com>
2024-04-14 14:57:16 +0530

    completed with upload feature

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,29 +1,24 @@
-import { React, useState } from "react";
+import { React, useState, useRef } from "react";
 import video from "../Images/Animation-1712172716816.webm";
 import axios from "axios";
 
 export const Form = () => {
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [imageUrl, setImageUrl] = useState('');
     const [prediction, setPrediction] = useState('');
+    const [videoVisible, setVideoVisible] = useState(true);
+    const [imageUrl, setImageUrl] = useState('');
+    const fileInputRef = useRef(null);
 
-    const fileSelectedHandler = event => {
-        setSelectedFile(event.target.files[0]);
-        const reader = new FileReader();
-        reader.onload = () => {
-            setImageUrl(reader.result);
-        };
-        reader.readAsDataURL(event.target.files[0]);
+    const handleUploadClick = () => {
+        fileInputRef.current.click();
     };
 
-    const fileUploadHandler = async () => {
+    const fileUploadHandler = async (event) => {
+        const selectedFile = event.target.files[0];
         try {
             const formData = new FormData();
             formData.append('file', selectedFile);
-            console.log(formData);
-            const URL = "https://74d3-34-82-123-154.ngrok-free.app/predict"
-
-            // Replace 'YOUR_BACKEND_URL' with the actual backend URL
+            const URL = "https://82fe-34-82-123-154.ngrok-free.app/predict"
+            // Replace 'YOUR_BACKEND_URL/predict' with the actual backend URL
             const response = await axios.post(URL, formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
@@ -34,26 +29,39 @@ export const Form = () => {
 
             // Set prediction result
             setPrediction(response.data.pred_class);
+            // Hide the video
+            setVideoVisible(false);
+
+            // Display uploaded image
+            const reader = new FileReader();
+            reader.onload = () => {
+                setImageUrl(reader.result);
+            };
+            reader.readAsDataURL(selectedFile);
         } catch (error) {
             console.error('Error:', error);
         }
     };
+
     return (
         <div className="box">
             <div className="hide">
-                <p>CHOOSE AN IMAGE</p>
-                {imageUrl && <video loop src={video} autoPlay={true} style={{ display: 'none' }}></video>}
-                <br />
-                <div>
-                    {imageUrl && <img src={imageUrl} alt="Uploaded" style={{ width: '200px', height: 'auto', marginTop: '20px' }} />}
-                    <input type="file" onChange={fileSelectedHandler} />
-                    <button onClick={fileUploadHandler}>Upload</button>
-                    {prediction && (
-                        <div>
-                            <p>Prediction: {prediction}</p>
-                        </div>
-                    )}
-                </div>
+                {videoVisible && (
+                    <video loop autoPlay style={{ width: '200px', height: 'auto', marginTop: '20px' }}>
+                        <source src={video} type="video/mp4" />
+                    </video>
+                )}
+                {videoVisible && (<p>CHOOSE AN IMAGE</p>)}
+                {imageUrl && (
+                    <img src={imageUrl} alt="Uploaded" style={{ width: '200px', height: 'auto', marginTop: '20px' }} />
+                )}
+                <input ref={fileInputRef} type="file" onChange={fileUploadHandler} accept="image/*" style={{ display: 'none' }} />
+                <button onClick={handleUploadClick}>Upload</button>
+                {prediction && (
+                    <div>
+                        <p>Prediction: {prediction}</p>
+                    </div>
+                )}
             </div>
             <div id="responseContainer"></div>
         </div>
