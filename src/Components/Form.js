9465ee44b042aa9017dae9765c9fc46180bfabc6
dheerajchhatanidc <100718766+dheerajchhatanidc@users.noreply.github.com>
2024-04-29 21:15:55 +0530

    Update Form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,70 +3,92 @@ import video from "../Images/Animation-1712172716816.webm";
 import axios from "axios";
 import { useTranslation } from "react-i18next";
 export const Form = () => {
-     const { t } = useTranslation();
-    const [prediction, setPrediction] = useState('');
-    const [Confidence, setConfidence] = useState('');
-    const [videoVisible, setVideoVisible] = useState(true);
-    const [imageUrl, setImageUrl] = useState('');
-    const fileInputRef = useRef(null);
+  const { t } = useTranslation();
+  const [prediction, setPrediction] = useState("");
+  const [Confidence, setConfidence] = useState("");
+  const [id, setId] = useState("");
+  const [videoVisible, setVideoVisible] = useState(true);
+  const [imageUrl, setImageUrl] = useState("");
+  const fileInputRef = useRef(null);
 
-    const handleUploadClick = () => {
-        fileInputRef.current.click();
-    };
+  const handleUploadClick = () => {
+    fileInputRef.current.click();
+  };
 
-    const fileUploadHandler = async (event) => {
-        const selectedFile = event.target.files[0];
-        try {
-            const formData = new FormData();
-            formData.append('file', selectedFile);
-            const URL = "https://1156-34-23-167-0.ngrok-free.app/predict";
-            // Replace 'YOUR_BACKEND_URL/predict' with the actual backend URL
-            const response = await axios.post(URL, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            });
+  const fileUploadHandler = async (event) => {
+    const selectedFile = event.target.files[0];
+    try {
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+      const URL = "https://f5d7-34-138-36-204.ngrok-free.app/predict";
+      // Replace 'YOUR_BACKEND_URL/predict' with the actual backend URL
+      const response = await axios.post(URL, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
 
-            console.log('Response:', response.data);
+      console.log("Response:", response.data);
 
-            // Set prediction result
-            setPrediction(response.data.pred_class);
-            setConfidence(response.data.pred_conf);
-            // Hide the video
-            setVideoVisible(false);
+      // Set prediction result
+      setPrediction(response.data.pred_class);
+      setConfidence(response.data.pred_conf);
+      setId(response.data.id)
+      // Hide the video
+      setVideoVisible(false);
 
-            // Display uploaded image
-            const reader = new FileReader();
-            reader.onload = () => {
-                setImageUrl(reader.result);
-            };
-            reader.readAsDataURL(selectedFile);
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
+      // Display uploaded image
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImageUrl(reader.result);
+      };
+      reader.readAsDataURL(selectedFile);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-    return (
-        <div className="box box1">
-            <div className="hide">
-                {videoVisible && (
-                    <video loop autoPlay style={{ width: '200px', height: 'auto', marginTop: '20px' }}>
-                        <source src={video} type="video/mp4" />
-                    </video>
-                )}
-                {videoVisible && (<p>{t('CHOOSE AN IMAGE')}</p>)}
-                {imageUrl && (
-                    <img src={imageUrl} alt="Uploaded" style={{ width: '200px', height: 'auto', marginTop: '20px' }} />
-                )}
-                <input ref={fileInputRef} type="file" onChange={fileUploadHandler} accept="image/*" style={{ display: 'none' }} />
-                <button onClick={handleUploadClick}>{t('Upload')}</button>
-                {prediction && (
-                    <div>
-                        <p>Prediction: {prediction}<br/> Confidence : {Confidence} </p>
-                    </div>
-                )}
-            </div>
-            <div id="responseContainer"></div>
-        </div>
-    )
-}
+  return (
+    <div className="box">
+      <div className="hide">
+        {videoVisible && (
+          <video
+            loop
+            autoPlay
+            style={{ width: "200px", height: "auto", marginTop: "20px" }}
+          >
+            <source src={video} type="video/mp4" />
+          </video>
+        )}
+        {videoVisible && <p>{t("CHOOSE AN IMAGE")}</p>}
+        {imageUrl && (
+          <img
+            src={imageUrl}
+            alt="Uploaded"
+            style={{ width: "200px", height: "auto", marginTop: "20px" }}
+          />
+        )}
+        <input
+          ref={fileInputRef}
+          type="file"
+          onChange={fileUploadHandler}
+          accept="image/*"
+          style={{ display: "none" }}
+        />
+        <button onClick={handleUploadClick}>{t("Upload")}</button>
+        {prediction && (
+          <div>
+            <p>
+              Prediction: {prediction}
+              <br /> Confidence : {Confidence}{" "}
+              <br /> Confidence : {Confidence}{" "}
+              <br /> Id : {id}{" "}
+              
+            </p>
+          </div>
+        )}
+      </div>
+      <div id="responseContainer"></div>
+    </div>
+  );
+};
